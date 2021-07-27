07b4cc99988ab8744d5e7c5cc0c9e6865bc81da4
It'z ArU <88024858+Aru-Ofc-git@users.noreply.github.com>
2021-07-27 13:45:54 +0600

    Add files via upload

---
+++ fhack.py
@@ -0,0 +1,119 @@
+# coded by ARU
+# uncompyle6 version 3.7.4
+# Python bytecode 2.7
+# Decompiled from: Python 2.7.18 (default, Mar 20 2021, 14:59:33) 
+# [GCC 4.2.1 Compatible Android (6454773 based on r365631c2) Clang 9.0.8 (https:/
+# Embedded file name: sumarr
+import requests, os, time, sys, smtplib
+blue = '\x1b[94m'
+lightblue = '\x1b[94m'
+red = '\x1b[91m'
+white = '\x1b[97m'
+green = '\x1b[93m'
+green = '\x1b[1;32m'
+cyan = '\x1b[96m'
+end = '\x1b[0m'
+yellow = '\n\n\x1b[1;93m'
+os.system('clear')
+fuck = red + " \xe2\x9a\xa0\xef\xb8\x8f Warning : We are not responsible if someone misuses it."
+notice = yellow + '                \xe2\x9c\xaaFacebook Account Hacking Tools\xe2\x9c\xaa                      '
+print ' \n \x1b[92m    8888      88888      88       88 \n   88     88   88    88   88       88 \n   88     88   88    88   88       88 \n   888888888   8888       88       88 \n   88     88   88  88        888888 \n   88     88   88    88        888 '
+print ''
+print ''
+print ' Noakhailla Mal.We Can Make System, We Can Fuck System '
+print ''
+print ''
+print 'Made By 1R13A14'
+print ''
+print ''
+
+def logop(z):
+    for word in z + '\n':
+        sys.stdout.write(word)
+        sys.stdout.flush()
+        time.sleep(0.02)
+
+
+print ''
+logop(' \x1b[92m\n|****************************************************| \n| Author   : Ariful Islam Arman                      |\n| Tool     : FB Hacker                               |\n| Version  : 1.0                                     |\n| Facebook : ArU                                     |\n| Link     : https://www.facebook.com/Aru.Ofc/       |\n| Coded By : ArU                                     |\n******************************************************')
+print ''
+print ''
+print ''
+logop(fuck)
+print ''
+logop(notice)
+print ''
+print ''
+print ''
+print ''
+username = raw_input(cyan + ' [\xe2\x9c\x93] Enter your FB ID or username:')
+print ''
+print ''
+print ("Your Target " + username) 
+print ''
+print ''
+
+from tqdm import tqdm
+loop = tqdm(total = 10000, position=0, leave=False)
+for i in range(10000) : loop.set_description("Cheacking User Info".format(i))
+loop.update(1)
+loop.close()
+
+print '' 
+print'' 
+
+from tqdm import tqdm
+loop = tqdm(total = 10000, position=0, leave=False)
+for k in range(10000) : loop.set_description("loading user info".format(k))
+loop.update(1)
+loop.close()
+
+print ''
+print ''
+logop (green + ' [\xe2\x9c\x93] Loaded User Info')
+print ''
+print ''
+
+from tqdm import tqdm
+loop = tqdm(total = 10000, position=0, leave=False)
+for m in range(10000) : loop.set_description("Cheacking Secority".format(m))
+loop.update(1)
+loop.close()
+print ''
+logop (blue + ' [\xe2\x9c\x93] secority chacked ')
+print ''
+print ''
+logop (red + ' ')
+print ''
+
+from tqdm import tqdm
+loop = tqdm(total = 50000, position=0, leave=False)
+for r in range(50000) : loop.set_description("Secority Hacking".format(r))
+loop.update(1)
+loop.close()
+
+print''
+logop(blue + '[\xe2\x9c\x93] secority hacked ')
+print''
+print ''
+print ''
+logop(blue + ' ')
+print''
+print''
+from tqdm import tqdm
+loop = tqdm(total = 50000, position=0, leave=False)
+for p in range(50000) : loop.set_description("Creacking Password".format(p))
+loop.update(1)
+loop.close()
+print ''
+logop (yellow + 'Hi! Nasa Howkar')
+print ''
+print ''
+logop (red + ' How Are You? ')
+print ''
+print ''
+logop (cyan + 'Ei Tool Diye Jodi Facebook Hack Kora Jeto Tahole Zuker Etto Milion Dolar Khoroc Kortece Cul Cirar jonno? ')
+print ''
+print''
+print''
+logop(green + '      Thanks MR. NASA HOCKER ')
